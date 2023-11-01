dc981de0a121f2c17129b6344b05fdabad02f2a6
Rob Martin <59516575+RobCMYK@users.noreply.github.com>
2023-10-31 19:12:19 -0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,39 @@
+$(function () {
+    //what radio value did they select?
+  
+    $("#formSubmit").on("click", (e) => {
+      //ii. Package the data
+      
+      e.preventDefault();
+    });
+  
+    $("input[type=radio]").on("change", function () {
+      // var $this = $(this);
+      // if ($this.is(":checked")) alert("a");
+  
+      //myFunctionToLoadinStuffInThatPlaceForIndia(someData);
+  
+      let radioChoice = $("input[type=radio]:checked").val(); // A or B
+  
+      if (radioChoice === "A") {
+        $("#letterWordsSelect")
+          .empty()
+          .append(`<option value="banana">A</option>`)
+          .append(`<option value="brains">AA</option>`)
+          .append(`<option value="bbq">AAA</option>`);
+      } else if (radioChoice === "B") {
+        //`<option value="${optValue}">${optText}</option>`
+  
+        $("#letterWordsSelect")
+          .empty()
+          .append(`<option value="banana">banana</option>`)
+          .append(`<option value="brains">brains</option>`)
+          .append(`<option value="bbq">bbq</option>`);
+      } else {
+        $("#letterWordsSelect")
+          .empty()
+          .append(`<option value="banana">pick a letter...A or B</option>`);
+      }
+    });
+  });
+  
